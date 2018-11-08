c616d886b5fd88a4f76a03efb27e6d02df11a865
Lecodeurfou <32478789+Lecodeurfou@users.noreply.github.com>
2018-11-08 16:38:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dwaygon/tap_blockv2/Game_Loop.java
@@ -0,0 +1,27 @@
+package com.example.dwaygon.tap_blockv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Game_Loop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game__loop);
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/dwaygon/tap_blockv2/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.dwaygon.tap_blockv2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(btnPlay);
+    }
+
+    private View.OnClickListener btnPlay = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Intent gameActivity = new Intent(MainActivity.this, Game_Loop.class);
+            startActivity(gameActivity);
+        }
+    };
+
+
+
+
+
+}
