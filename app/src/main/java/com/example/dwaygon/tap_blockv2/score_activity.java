c4b0dbeaaf8efa08203ff33d35e7a8296b1f3d7d
Lecodeurfou <32478789+Lecodeurfou@users.noreply.github.com>
2018-11-14 15:31:38 +0100

    Add Score feature
    Ajout de deux boutons un replay et un retour menu sur la page de score + ajout d'un background.

--- app/src/main/java/com/example/dwaygon/tap_blockv2/score_activity.java
+++ app/src/main/java/com/example/dwaygon/tap_blockv2/score_activity.java
@@ -1,14 +1,19 @@
 package com.example.dwaygon.tap_blockv2;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class score_activity extends AppCompatActivity {
 
-    @SuppressLint("SetTextI18n")
+    private ImageButton btn_backmenu;
+    private ImageButton btn_replay;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +29,18 @@ public class score_activity extends AppCompatActivity {
             TextView view = findViewById(R.id.score_text);
             TextView view_score= findViewById(R.id.score_text_number);
 
+
+            btn_replay =(ImageButton) findViewById(R.id.imageButton_replay);
+
+
+
+
             if (score == 0) { //Boss
 
                 if(score_boss>=5000){
                     view.setText("Vous avez été défait par votre enemi ...");
                     //add image etc ...
+                    
                 }
                 else{
                     view.setText("Vous avez défait votre enemi, félicitation !");
@@ -36,16 +48,53 @@ public class score_activity extends AppCompatActivity {
 
                 }
 
+                btn_replay.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent gameActivity = new Intent(score_activity.this, game_loop_boss.class);
+                        startActivity(gameActivity);
+                    }
+                });
+
+
 
             }
             if (score != 0) { //Arcade
                 view.setText("Score : ");
                 view_score.setText(Integer.toString(score));
 
+
+
+                btn_replay.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent gameActivity = new Intent(score_activity.this, game_loop.class);
+                        startActivity(gameActivity);
+                    }
+                });
+
+
+
+
+
             }
 
         }
 
+
+
+
+
+        btn_backmenu =(ImageButton) findViewById(R.id.imageButton_backmenu);
+
+        btn_backmenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  finish();
+                Intent gameActivity = new Intent(score_activity.this, MainActivity.class);
+                startActivity(gameActivity);
+            }
+        });
 
 
 
